22b99d70fdd1ebb979c9d361c3a20b578b9e4418
Riakhin Oleksandr <ryakhin.ua@gmail.com>
2025-02-17 15:02:54 +0200

    Lab 1, final changes

--- 1/task.py
+++ 1/task.py
@@ -3,18 +3,26 @@ from multiprocessing import Process
 import time
 import random
 
-def reset_counter(connection, cursor, version = False, id = 1):
+def reset_counter(connection, cursor, version = False, id = 1, rand = False):
     """ Resetting Counter and Version(if version parameter is True) values in database"""
-    cursor.execute("UPDATE lab1 SET Counter = 0 WHERE USER_ID = %s;", (id,))
-    if version:
-        cursor.execute("UPDATE lab1 SET Version = 1 WHERE USER_ID = %s;", (id,))
+    if rand:
+        for i in range(1, 100001):
+            cursor.execute("UPDATE lab1 SET Counter = 0 WHERE USER_ID = %s;", (i,))
+            if version:
+                cursor.execute("UPDATE lab1 SET Version = 1 WHERE USER_ID = %s;", (i,))
+    else:
+        cursor.execute("UPDATE lab1 SET Counter = 0 WHERE USER_ID = %s;", (id,))
+        if version:
+            cursor.execute("UPDATE lab1 SET Version = 1 WHERE USER_ID = %s;", (id,))
     connection.commit()
 
-def lost_update(id = 1):
+def lost_update(id = 1, rand = False):
     """ Lost-update variation """
     conn = psycopg2.connect(database = "bd", user = "postgres", password = "12345678")
     cur = conn.cursor()
     for i in range(1, 10001):
+        if rand:
+            id = random.randint(1, 100000)
         cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = %s", (id,))
         counter = cur.fetchone()[0]
         counter +=  1
@@ -23,21 +31,25 @@ def lost_update(id = 1):
     cur.close()
     conn.close()
 
-def inplace_update(id = 1):
+def inplace_update(id = 1, rand = False):
     """ In-place variation """
     conn = psycopg2.connect(database = "bd", user = "postgres", password = "12345678")
     cur = conn.cursor()
     for i in range(1, 10001):
+        if rand:
+            id = random.randint(1, 100000)
         cur.execute("UPDATE lab1 SET Counter = Counter + 1 WHERE USER_ID = %s", (id, ))
         conn.commit()
     cur.close()
     conn.close()
 
-def row_level_locking(id = 1):
+def row_level_locking(id = 1, rand = False):
     """ Row-level locking variation """
     conn = psycopg2.connect(database = "bd", user = "postgres", password = "12345678")
     cur = conn.cursor()
     for i in range(1, 10001):
+        if rand:
+            id = random.randint(1, 100000)
         cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = %s FOR UPDATE", (id,))
         counter = cur.fetchone()[0]
         counter +=  1
@@ -46,11 +58,13 @@ def row_level_locking(id = 1):
     cur.close()
     conn.close()
 
-def optimistic_concurency_control(id = 1):
+def optimistic_concurency_control(id = 1, rand = False):
     """ Optimistic concurrency control variation """
     conn = psycopg2.connect(database = "bd", user = "postgres", password = "12345678")
     cur = conn.cursor()
     for i in range(1, 10001):
+        if rand:
+            id = random.randint(1, 100000)
         while True:
             cur.execute("SELECT Counter, Version FROM lab1 WHERE USER_ID = %s", (id,))
             counter, version = cur.fetchone()
@@ -64,7 +78,7 @@ def optimistic_concurency_control(id = 1):
     conn.close
 
 if __name__ == '__main__':
-    big_data = False # Set False if there will be only one row. Set True if there will be 100000 and id will be chosen randomly
+    rand = False # Set False if there will be only one row. Set True if there will be 100000 and id will be chosen randomly
 
     # Preparing
     id = 1
@@ -77,11 +91,9 @@ if __name__ == '__main__':
         cur.execute("INSERT INTO lab1 VALUES (1, 0, 1);") # Adding row if table is empty
     else:
         cur.execute("UPDATE lab1 SET Counter = 0, Version = 1 WHERE USER_ID = %s;", (id,)) # Resetting Counter and Version 
-    if big_data:
-        id = random.randint(1, 100000)
+    if rand:
         for i in range(2, 100001):
             cur.execute("INSERT INTO lab1 VALUES (%s, 0, 1);", (i,))
-        print(f"Id is {id}\n")
     conn.commit()
 
     # Варіант Lost-update
@@ -89,7 +101,7 @@ if __name__ == '__main__':
 
     processes = []
     for i in range(10):
-        p = Process(target=lost_update, args=(id,)) # Creating processes and setting their target
+        p = Process(target=lost_update, args=(id, rand)) # Creating processes and setting their target
         processes.append(p)
         p.start() # Starting processes
     
@@ -101,17 +113,21 @@ if __name__ == '__main__':
     timer = round(end - start, 5)
 
     print(f"Lost-update variant took {timer} seconds")
-    cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = %s", (id,))
-    print(f"Counter: {cur.fetchone()[0]}\n")
+    if rand:
+        cur.execute("SELECT SUM(Counter) FROM lab1;")
+        print(f"Total Counter: {cur.fetchone()[0]}\n")
+    else:
+        cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = 1;")
+        print(f"Counter: {cur.fetchone()[0]}\n")
 
-    reset_counter(conn, cur, id = id)
+    reset_counter(conn, cur, id = id, rand = rand)
 
     # Варіант In-place update
     start = time.time()
 
     processes = []
     for i in range(10):
-        p = Process(target=inplace_update, args=(id,)) # Creating processes and setting their target
+        p = Process(target=inplace_update, args=(id, rand)) # Creating processes and setting their target
         processes.append(p)
         p.start() # Starting processes
     
@@ -123,17 +139,21 @@ if __name__ == '__main__':
     timer = round(end - start, 5)
 
     print(f"In-place update variant took {timer} seconds")
-    cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = %s", (id,))
-    print(f"Counter: {cur.fetchone()[0]}\n")
+    if rand:
+        cur.execute("SELECT SUM(Counter) FROM lab1;")
+        print(f"Total Counter: {cur.fetchone()[0]}\n")
+    else:
+        cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = 1;")
+        print(f"Counter: {cur.fetchone()[0]}\n")
 
-    reset_counter(conn, cur, id = id)
+    reset_counter(conn, cur, id = id, rand = rand)
 
     # Варіант Row-level locking
     start = time.time()
 
     processes = []
     for i in range(10):
-        p = Process(target=row_level_locking, args=(id,)) # Creating processes and setting their target
+        p = Process(target=row_level_locking, args=(id, rand)) # Creating processes and setting their target
         processes.append(p)
         p.start() # Starting processes
     
@@ -145,17 +165,21 @@ if __name__ == '__main__':
     timer = round(end - start, 5)
 
     print(f"Row-level locking variant took {timer} seconds")
-    cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = %s", (id,))
-    print(f"Counter: {cur.fetchone()[0]}\n")
+    if rand:
+        cur.execute("SELECT SUM(Counter) FROM lab1;")
+        print(f"Total Counter: {cur.fetchone()[0]}\n")
+    else:
+        cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = 1;")
+        print(f"Counter: {cur.fetchone()[0]}\n")
 
-    reset_counter(conn, cur, id = id)
+    reset_counter(conn, cur, id = id, rand = rand)
 
     # Варіант Optimistic concurrency control
     start = time.time()
 
     processes = []
     for i in range(10):
-        p = Process(target=optimistic_concurency_control, args=(id,)) # Creating processes and setting their target
+        p = Process(target=optimistic_concurency_control, args=(id, rand)) # Creating processes and setting their target
         processes.append(p)
         p.start() # Starting processes
     
@@ -167,13 +191,16 @@ if __name__ == '__main__':
     timer = round(end - start, 5)
 
     print(f"Optimistic concurrency control variant took {timer} seconds")
-    cur.execute("SELECT Counter, Version FROM lab1 WHERE USER_ID = %s", (id,))
-    counter, version = cur.fetchone()
-    print(f"Counter: {counter}\nVersion: {version}\n")
+    if rand:
+        cur.execute("SELECT SUM(Counter) FROM lab1;")
+        print(f"Total Counter: {cur.fetchone()[0]}\n")
+    else:      
+        cur.execute("SELECT Counter FROM lab1 WHERE USER_ID = 1;")
+        print(f"Counter: {cur.fetchone()[0]}\n")
 
-    reset_counter(conn, cur, version = True, id = id)
+    reset_counter(conn, cur, version = True, id = id, rand = rand)
 
-    if big_data:
+    if rand:
         cur.execute("DELETE FROM lab1 WHERE USER_ID > 1;")
         conn.commit()
 
